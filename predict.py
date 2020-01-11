75c004d2167290987099335653a2e25f1e652cc6
DimChristodoulou <dchristodoulou.di@gmail.com>
2020-01-11 15:21:16 +0200

    completed first question

---
+++ predict.py
@@ -0,0 +1,46 @@
+import keras
+import numpy as np
+import pandas as pd
+import sys
+from keras import layers, optimizers, losses, metrics
+from keras.models import load_model
+
+# General Configurations
+keras.backend.set_epsilon(1e-2)
+np.set_printoptions(threshold=sys.maxsize)
+
+# Read files
+actualData = pd.read_csv('Datasets/actual.csv')
+testingData = pd.read_csv('Datasets/nn_representations.csv')
+
+actualDataLabels = actualData.iloc[:, :1]
+actualDataValues = actualData.iloc[:, 1:]
+actualDataValues = actualDataValues.to_numpy()
+
+testingDataLabels = testingData.iloc[:, :1]
+testingDataValues = testingData.iloc[:, 1:]
+
+result = pretrainedmodel.predict(testingDataValues)
+
+# Generate MAE Loss
+mae = keras.losses.MeanAbsoluteError()
+loss = mae(actualDataValues, result)
+print('MAE Loss: ', loss.numpy())  # Loss: 0.75
+
+# Generate MSE Loss
+mse = keras.losses.MeanSquaredError()
+loss = mse(actualDataValues, result)
+print('MSE Loss: ', loss.numpy())  # Loss: 0.75
+
+# Generate MAPE Loss
+mape = keras.losses.MeanAbsolutePercentageError()
+loss = mape(actualDataValues, result)
+print('MAPE Loss: ', loss.numpy())  # Loss: 5e+08
+
+# actualDataValuesColumns = list()
+# for column in actualDataValues.T:
+#     actualDataValuesColumns.append(column)
+
+# resultColumns = list()
+# for column in result.T:
+#     resultColumns.append(column)
